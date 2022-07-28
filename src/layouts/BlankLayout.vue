a26fec5dd4d4a19b064d1e7177fcaa7a497d2a81
guochang <909753361@qq.com>
2022-07-28 11:23:07 +0800

    feat:new fun

---
+++ src/layouts/BlankLayout.vue
@@ -0,0 +1,16 @@
+<script lang='ts'>
+import { defineComponent } from "vue";
+export default defineComponent({
+    setup() {
+        return {};
+    },
+    components: {},
+});
+</script>
+
+<template>
+    <router-view></router-view>
+</template>
+
+<style scoped lang='less'>
+</style>
---
+++ src/layouts/IndexLayout.vue
@@ -0,0 +1,19 @@
+<script lang='ts'>
+import { defineComponent } from "vue";
+import Footer from "@/components/footer/footer.vue";
+export default defineComponent({
+    setup() {
+        return {};
+    },
+    components: { Footer }
+});
+</script>
+
+<template>
+    <div>Header Content</div>
+    <router-view></router-view>
+    <div>Footer Content</div>
+</template>
+
+<style scoped lang='less'>
+</style>
