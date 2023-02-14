50a55086ce099c0be41c61a1d1388855961ca2de
guochang <909753361@qq.com>
2023-02-14 13:51:01 +0800

    zxczxczxczxczxzxczxc

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "@/permission.js";
+import 'element-plus/theme-chalk/dark/css-vars.css'
+
 // 自定义全局css
 // import "./assets/style/main.less";
 import './index.css'
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,27 @@
 <script lang="ts" setup>
-import { onMounted } from "vue";
+import { onMounted ,ref} from "vue";
+import {useDark, useToggle} from '@vueuse/core'
 onMounted(() => {});
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+const tableData = ref<any>([])
 </script>
 
 <template>
-  <div class="grid grid-cols-3 w-64 bg-red-400 p-5">
-    <div class="w-6 bg-yellow-500">1</div>
-    <div class="w-6 bg-green-500">2</div>
-    <div class="w-6 bg-blue-500">3</div>
+  <div class="p-5 h-full">
+    <!-- element的主题改变有用 -->
+    <el-button @click="toggleDark()">默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="info">info按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+  </div>
+  <div>
+    <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table>
   </div>
 </template>
 
