4727d5e46d0ad4bcb67f4cf92439b94e9611b5f5
guochang <909753361@qq.com>
2024-07-12 16:42:05 +0800

    fix:fix some bugs

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,30 @@
 <template>
   <!-- <div v-loading="loading" :element-loading-text="tips"> -->
-    <n-config-provider
-      :locale="zhCN"
-      :date-locale="dateZhCN"
-      :theme-overrides="themeOverrides"
-    >
-      <n-notification-provider>
-        <n-dialog-provider>
-          <router-view />
-        </n-dialog-provider>
-      </n-notification-provider>
-    </n-config-provider>
+  <n-config-provider
+    :locale="zhCN"
+    :date-locale="dateZhCN"
+    :theme-overrides="themeOverrides"
+  >
+    <n-notification-provider>
+      <n-dialog-provider>
+        <router-view />
+      </n-dialog-provider>
+    </n-notification-provider>
+  </n-config-provider>
   <!-- </div> -->
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { zhCN, dateZhCN, GlobalThemeOverrides } from "naive-ui";
 
-export default defineComponent({
-  setup() {
-
-    const loading = ref(false);
-    const tips = ref("");
-    const themeOverrides: GlobalThemeOverrides = {
-      common: {
-        primaryColor: "#5983b8",
-        primaryColorHover: "#79a4da",
-        primaryColorPressed: "#426897",
-      },
-    };
-    return {
-      zhCN,
-      dateZhCN,
-      loading,
-      tips,
-      themeOverrides,
-    };
+const loading = ref(false);
+const tips = ref("");
+const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    primaryColor: "#5983b8",
+    primaryColorHover: "#79a4da",
+    primaryColorPressed: "#426897",
   },
-});
+};
 </script>
-<style>
-</style>
+<style></style>
