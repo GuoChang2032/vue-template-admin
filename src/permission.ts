34e9a08a9dd1f14b9b561d2babea2349682076ca
GuoChang2032 <qweasdzxc102@qq.com>
2025-08-25 15:48:24 +0800

    优化项目标题名称

--- src/permission.ts
+++ src/permission.ts
@@ -1,31 +1,31 @@
-import router from './router'
-import { start, done } from '@/utils/nprogress.js'
-import { useUserInfo } from "@/stores/user"
+import router from "./router";
+import { start, done } from "@/utils/nprogress.js";
+import { useUserInfo } from "@/stores/user";
 
-const whiteList = ['/login'] // no redirect whitelist
-const APP_NAME = ' - 前端模板项目'
+const whiteList = ["/login"]; // no redirect whitelist
+const APP_NAME = import.meta.env.VITE_APP_NAME;
 
 // 路由跳转前
 router.beforeEach((to, from, next) => {
-    start()
-    const user = useUserInfo()
-    if (to.meta.title) {
-        document.title = to.meta.title + APP_NAME
-    } else {
-        document.title = '前端模板项目'
-    }
+  start();
+  const user = useUserInfo();
+  if (to.meta.title) {
+    document.title = to.meta.title + " - " + APP_NAME;
+  } else {
+    document.title = APP_NAME;
+  }
 
-    // let token = user.token
-    // if (!token && !whiteList.includes(to.path)) {
-    //     done()
-    //     Message('warning', '请登录')
-    //     next({ path: '/login' })
-    //     return
-    // }
-    done()
-    next()
-})
+  // let token = user.token
+  // if (!token && !whiteList.includes(to.path)) {
+  //     done()
+  //     Message('warning', '请登录')
+  //     next({ path: '/login' })
+  //     return
+  // }
+  done();
+  next();
+});
 // 路由跳转后
 router.afterEach(() => {
-    done()
-})
+  done();
+});
